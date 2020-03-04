c43ee3e8a3cbd790a65e7623ab5435823fd423ce
314893404@qq.com <GitHub123>
2020-03-04 23:57:08 +0800

    未配置spring config，

---
+++ springcloud-config-server-3344/src/main/java/com/hanbo/springcloud/ConfigServer3344.java
@@ -0,0 +1,15 @@
+package com.hanbo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServer3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer3344.class, args);
+    }
+
+}
